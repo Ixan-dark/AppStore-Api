fe6cb74286c9e945cf32fbd36c2c0d25c3148b46
ixan dark <ixan0553@gmail.com>
2025-12-07 20:49:57 +0000

    server fire update

--- src/server.js
+++ src/server.js
@@ -8,20 +8,23 @@ import { dbConnect } from '../dbConnect/dbConnect.js';
 
 dotenv.config();
 
-
 const app = express();
 const PORT = 3000;
 
-// Middleware setup
+
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
 
-// Routes setup
-app.use('/api/apps',appRoutes);
 
-// Start the server
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+
+app.use('/api/apps', appRoutes);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-    dbConnect();
+  dbConnect();
 });
